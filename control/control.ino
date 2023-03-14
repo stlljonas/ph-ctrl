0d024276ca746e2523c2713f5f719b7c852c9227
jonas <stlljonas@gmail.com>
2023-03-14 12:30:07 +0100

    Tuning

--- control/control.ino
+++ control/control.ino
@@ -7,21 +7,26 @@ int sensorValue = 0;
 unsigned long int avgValue;
 float b;
 int buf[10],temp;
-int loop_delay_ms = 1000; //10 * 60 * 1000;
-int control_input_length_ms = 3000;
-float margin_of_error = 0.2;
-float ph_max = 6.5 + margin_of_error;
-float ph_min = 5.5 - margin_of_error;
+unsigned long int loop_delay_ms = 5 * 60000;
+unsigned long int control_input_length_ms = 3000;
+float margin_of_error = 0.3;
+float ph_max = 6.0 + margin_of_error; // optimal: 6.5
+float ph_min = 6.0 - margin_of_error; // optimal: 5.5
+unsigned long int last_timestamp = 0;
  
 void setup() {
   Serial.begin(9600);
   pinMode(MOTOR1_IN1, OUTPUT);
   pinMode(MOTOR2_IN1, OUTPUT);
+  Serial.println("Startup: Waiting for pH value to settle");
 }
 
 void loop() {
-
-  for(int i=0;i<10;i++) {
+  delay(10000);
+  if (millis() >= last_timestamp + loop_delay_ms){
+    Serial.println("checking sensor");
+    last_timestamp = millis();
+    for(int i=0;i<10;i++) {
       buf[i]=analogRead(analogInPin);
       delay(30);
     }
@@ -34,51 +39,52 @@ void loop() {
         }
       }
     }
-  avgValue=0;
-  for(int i=2;i<8;i++)
-  avgValue+=buf[i];
-  float pHVol=(float)avgValue*5.0/1024/6;
-  float phValue = -5.70 * pHVol + calibration;
-  Serial.print("sensor = ");
-  Serial.println(phValue);
+    avgValue=0;
+    for(int i=2;i<8;i++)
+    avgValue+=buf[i];
+    float pHVol=(float)avgValue*5.0/1024/6;
+    float phValue = -5.70 * pHVol + calibration;
+    Serial.print("sensor:");
+    Serial.println(phValue);
 
-  if (phValue > ph_max) {
-    Serial.println("control input: phDown");
-    // ramp up forward
-    digitalWrite(MOTOR1_IN1, LOW);
-    for (int i=0; i<255; i++) {
-      analogWrite(MOTOR1_IN1, i);
-      delay(10);
-    }
+    if (phValue > ph_max) {
+      Serial.println("control input: phDown");
+      // ramp up forward
+      digitalWrite(MOTOR1_IN1, LOW);
+      for (int i=0; i<255; i++) {
+        analogWrite(MOTOR1_IN1, i);
+        delay(10);
+      }
 
-    // forward full speed for 3 seconds
-    delay(control_input_length_ms);
-    
-    // ramp down forward
-    for (int i=255; i>=0; i--) {
-      analogWrite(MOTOR1_IN1, i);
-      delay(10);
-    }
-  } else if (phValue < ph_min) {
-    Serial.println("control input: phUp");
+      // forward full speed for 3 seconds
+      delay(control_input_length_ms);
+      
+      // ramp down forward
+      for (int i=255; i>=0; i--) {
+        analogWrite(MOTOR1_IN1, i);
+        delay(10);
+      }
+    } else if (phValue < ph_min) {
+      Serial.println("control input: phUp");
 
-    // ramp up forward
-    digitalWrite(MOTOR2_IN1, LOW);
-    for (int i=0; i<255; i++) {
-      analogWrite(MOTOR2_IN1, i);
-      delay(10);
-    }
+      // ramp up forward
+      digitalWrite(MOTOR2_IN1, LOW);
+      for (int i=0; i<255; i++) {
+        analogWrite(MOTOR2_IN1, i);
+        delay(10);
+      }
 
-    delay(control_input_length_ms);
-    
-    // ramp down forward
-    for (int i=255; i>=0; i--) {
-      analogWrite(MOTOR2_IN1, i);
-      delay(10);
+      delay(control_input_length_ms);
+      
+      // ramp down forward
+      for (int i=255; i>=0; i--) {
+        analogWrite(MOTOR2_IN1, i);
+        delay(10);
+      }
+    } else {
+      Serial.println("control input: None");
     }
-  } else {
-    Serial.println("control input: None");
+    Serial.println("Sleeping for about five minute.");
   }
-
-  delay(loop_delay_ms);
+  // delay(loop_delay_ms);s
 }
